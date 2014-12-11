102dd4593934a645fa6db0723d925c9828b18c75
Adam Riggs <adam@adamriggs.com>
2014-12-11 00:32:23 -0500

    tried the code from the googlechrome githup page but it didn't work

--- A-GetUserMedia/js/main.js
+++ A-GetUserMedia/js/main.js
@@ -1,4 +1,27 @@
 'use strict'
+// // variables in global scope so available to console
+// var video = document.querySelector('video');
+// var constraints = {
+//   audio: false,
+//   video: true
+// };
+// 
+// navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+// 
+// function successCallback(stream) {
+//   window.stream = stream; // stream available to console
+//   if (window.URL) {
+//     video.src = window.URL.createObjectURL(stream);
+//   } else {
+//     video.src = stream;
+//   }
+// }
+// 
+// function errorCallback(error) {
+//   console.log('navigator.getUserMedia error: ', error);
+// }
+// 
+// navigator.getUserMedia(constraints, successCallback, errorCallback);
 
 var app = null;
 
